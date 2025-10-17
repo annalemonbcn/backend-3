ada80c8a688f15c6c1ce0b2f6b82665a4cde2402
Anna Esteve <aesteve8@gmail.com>
2025-10-17 16:41:04 +0200

    dockerfile created

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 8080;
 const startServer = async () => {
   try {
     await connectToDatabase();
-    app.listen(PORT, () =>
+    app.listen(PORT, '0.0.0.0', () =>
       console.log(`🚀 Server running on http://localhost:${PORT}`)
     );
   } catch (err) {
